82d566f83c38630460af7047c425bf451a6ca0c0
Daddy Awesome <sablay296@gmail.com>
2022-05-14 11:20:37 +0800

    wala an save gahapon

---
+++ codingExercise304.py
@@ -0,0 +1,18 @@
+class Costumers:
+    greeting = "Welcome to the Coffee Palace"
+
+c_1 = Costumers()
+c_1.name = "Samirah" 
+c_1.beverage = "Iced caffe latte"  
+c_1.food = "Cinnamon roll" 
+c_1.total = 225 
+
+c_2 = Costumers()
+c_2.name = "Jerry"
+c_2.beverage ="Caramel macchiato"
+c_2.food = "Glazed doughnut"
+c_2.total = 230
+
+print(c_2.greeting)
+print(c_1.beverage)
+print(c_1.beverage)
